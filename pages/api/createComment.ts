08c0ae50cd90297b8e209b9557f406afff70e9d9
Artem <artem.bqpower@gmail.com>
2022-10-23 22:35:35 +0300

    finish sanity

--- pages/api/createComment.ts
+++ pages/api/createComment.ts
@@ -4,10 +4,10 @@ import sanityClient from '@sanity/client'
 
 
 const config = {
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    token: process.env.SANITY_API_TOKEN,
     useCdn: process.env.NODE_ENV === 'production',
-    token: process.env.SANITY_API_TOKEN
 }
 
 const client = sanityClient(config)
